a2c2fca00669819ddcd7f69caf2efbe7b837d921
FlavioLAMartins <flavio_martins11@hotmail.com>
2022-12-10 15:40:57 -0300

    add registerAPI

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"deposito/handler"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	handler.ConfigServer()
+	r := mux.NewRouter().StrictSlash(true)
+	handler.RegisterAPI(r)
+	fmt.Println("Server on :5000 ")
+	log.Fatal(http.ListenAndServe(":5000", r))
 
 }
--- handler/router.go
+++ handler/router.go
@@ -1,35 +1,26 @@
 package handler
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
 )
 
-func ConfigServer() {
+func RegisterAPI(r *mux.Router) {
+
 	n := negroni.Classic()
 	n.Use(applicationJSON())
 	n.Use(basicAuth())
-	r := mux.NewRouter().StrictSlash(true)
+
 	n.UseHandler(r)
-	RegisterAPI(r)
-	fmt.Println("Server on :5000 ")
-	log.Fatal(http.ListenAndServe(":5000", r))
-}
 
-func RegisterAPI(router *mux.Router) {
+	r.HandleFunc("/products", CreateProduct).Methods("POST")
 
-	router.HandleFunc("/products", CreateProduct).Methods("POST")
+	r.HandleFunc("/products", GetAllProducts).Methods("GET")
 
-	router.HandleFunc("/products", GetAllProducts).Methods("GET")
+	r.HandleFunc("/products/{id}", GetById).Methods("GET")
 
-	router.HandleFunc("/products/{id}", GetById).Methods("GET")
+	r.HandleFunc("/products/{id}", DeleteById).Methods("DELETE")
 
-	router.HandleFunc("/products/{id}", DeleteById).Methods("DELETE")
-
-	router.HandleFunc("/products/{id}", UpdateById).Methods("PUT")
+	r.HandleFunc("/products/{id}", UpdateById).Methods("PUT")
 
 }
